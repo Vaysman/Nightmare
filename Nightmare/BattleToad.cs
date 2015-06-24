c761e4caa73187427bede7beb1555bb67fbf8096
Mikhail Vaysman <mvaisman@spb-MVaisman.spb.luxoft.com>
2015-06-24 20:30:40 +0300

    remove c# files. update .gitignore

--- Nightmare/BattleToad.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nightmare {
-    class BattleToad : Beast {
-        public BattleToad() {
-            BaseAttack = 4;
-            Defence = 2;
-            IsPoison = 1;
-        }
-    }
-}
--- Nightmare/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nightmare {
-    /// <summary>
-    /// Базовый класс для всех предметов в игре
-    /// </summary>
-    public abstract class Item {
-        public string Name;
-        public string Type;
-    }
-}
